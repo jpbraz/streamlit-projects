2673019c4402252477f72f7964528f1f8f2ce121
João Paulo Braz <jparaujobraz84@gmail.com>
2024-07-09 18:40:13 -0300

    ajustes

--- app2.py
+++ app2.py
@@ -33,7 +33,7 @@ if file_a and file_b:
     df_a.dropna(subset=['Lançamento'], inplace=True)
 
     # Filtrar df_a com os pagamentos realizados do Grupo igual a VENDA CARTEIRA DIGITAL ou VENDA CARTÕES
-    df_a = df_a[df_a['Grupo'].isin(['VENDA CARTEIRA DIGITAL', 'VENDA CARTÕES'])]
+    df_a = df_a[df_a['Grupo'].isin(['VENDA CARTEIRA DIGITAL', 'ZZZ-VENDA CARTÕES'])]
     df_a['checked'] = False
 
     df_b['checked'] = False
@@ -62,10 +62,8 @@ if file_a and file_b:
     def merge_condition(row1, row2):
         time_diff = np.abs((row1[datetime_a] - row2['datetime_b']).total_seconds() / 60)
         value_diff = np.abs(row1[valor_a] - row2[valor_b])
-        if value_diff == 0.0:
-          print(time_diff, value_diff)
-        
-        return time_diff <= 30000 and value_diff == 0.0 and row1['checked'] == False and row2['checked'] == False
+        # Se a diferença de tempo for menor ou igual a 45 minutos e checked não estiver marcado, então retorna True
+        return value_diff == 0.0 and time_diff <= 45 and row1['checked'] == False and row2['checked'] == False
 
 
     # Realizando o merge
@@ -94,59 +92,27 @@ if file_a and file_b:
 
     print(merged_df)
 
-    # # Criar uma coluna para indicar se o pagamento foi efetivado
-    # def match_payment(row_a, df_b):
-    #     # atribuir valor para valor_a baseado no valor de C. Digital. Se C. Digital > 0.0 usar o valor da coluna C. Digital, senão o valor da coluna Cartão     
-    #     valor_a = 'Cartão' if row_a['C. Digital'] == '0.00' else 'C. Digital'
-    #     timestamp_a = round(row_a[datetime_a].timestamp())
-    #     # timestamp_b = round(df_b['datetime_b'] .timestamp())
-    #     # print(f'{timestamp_a} - {timestamp_b} - {timedelta(minutes=40)}')
-    #     mask = (
-    #         #(abs(pd.to_datetime(row_a[datetime_a]) - df_b['datetime_b']) <= timedelta(minutes=40)) &
-    #         (df_b[valor_b] == row_a[valor_a])
-    #     )
-    #     # imprimir o valor de datetime_b correspondente ao mask
-    #     if mask.any():
-    #       print(df_b[mask]['datetime_b'], df_b[mask][valor_b], df_b[mask]['pagador'])
-    #     # return df_b[mask]['datetime_b'].values[0] if mask.any() else None  # retorna a primeira data de datetime_b que corresponde ao mask
-    #     # return df_b[mask][valor_b].values[0] if mask.any() else 0  # retorna 0 se não houver um match no df_b
-    #     # return df_b[mask][valor_b].values[0] if mask.any() else pd.NaT  # retorna NaT se não houver um match no df_b
-        
-    #     return mask.any()
-    
-    # # Função para verificar se dois pagamentos são correspondentes
-    # def match_payment(row_a, df_b):
-    #     mask = (
-    #         #(abs(row_a[datetime_a] - df_b['datetime_b']) <= timedelta(minutes=30)) &
-    #         (df_b[valor_b] == row_a[valor_a])
-    #     )
-    #     return df_b[mask]
-
-    # # Aplicar a função de correspondência
-    # df_a['efetivado'] = df_a.apply(lambda row: match_payment(row, df_b), axis=1)
-
-    
-
-
     # Mostrar DataFrame
     st.write("Planilha A (Pagamentos Recebidos):")
     st.dataframe(df_a)
+    st.write(f"Total: {len(df_a)}")
 
     st.write("Planilha B (Pagamentos Compensados):")
     st.dataframe(df_b)
+    st.write(f"Total: {len(df_b)}")
 
-    st.write("MERGED:")
+    st.write("CONSILIAÇÃO:")
     st.dataframe(merged_df)
 
     pagamentos_efetivados = df_a[df_a['checked']]
     pagamentos_nao_efetivados = df_a[~df_a['checked']]
 
     # Exibir os resultados
-    st.write(f"Pagamentos Efetivados: {len(pagamentos_efetivados)}")
-    st.write(f"Pagamentos Não Efetivados: {len(pagamentos_nao_efetivados)}")
+    st.write(f"Pagamentos Encontrados: {len(pagamentos_efetivados)}")
+    st.write(f"Pagamentos Não Encontrados: {len(pagamentos_nao_efetivados)}")
 
-    st.write("Pagamentos Não Efetivados (Detalhes):")
+    st.write("Pagamentos Não Encontrados (Detalhes):")
     st.dataframe(pagamentos_nao_efetivados)
 
-    st.write("Pagamentos Efetivados (Detalhes):")
+    st.write("Pagamentos Encontrados (Detalhes):")
     st.dataframe(pagamentos_efetivados)
